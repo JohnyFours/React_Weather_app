2586b4074066a6724d5010019327064afe3f198d
Oleksandr Protasov <aleks.protasov@outlook.com>
2023-01-20 09:58:53 +0200

    add weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
+import { WEATHER_API_KEY, WEATHER_API_URL } from './api';
 import './App.css';
+import CurrentWeather from './components/currentWeather/currentWeather';
+import Forecast from './components/forecast/forecast';
+import Search from './components/search/Search';
 
 function App() {
+
+  const [currenWeather, setCurrenWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currenWeaterFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
+    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
+
+    Promise.all([currenWeaterFetch, forecastFetch])
+      .then(async (resopnse) => {
+        const weatherResponse = await resopnse[0].json();
+        const forecastResponse = await resopnse[1].json();
+        setCurrenWeather({city: searchData.label, ...weatherResponse});
+        setForecast({city: searchData.label, ...forecastResponse});
+      })
+      .catch((err)=> console.log(err));
+  }
+  console.log(forecast);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
+      {currenWeather && <CurrentWeather data ={currenWeather}/>}
+      {forecast && <Forecast data={forecast}/>}
     </div>
   );
 }
